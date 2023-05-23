bd7aa64c60e15db4cde781b4cb23f557a319918b
ze_bnu@hotmail.com <ze_bnu@hotmail.com>
2023-05-23 04:49:17 -0300

    Subindo código base do projeto

---
+++ MyContactsBackEnd/Api/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "feito get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "feito post";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "feito put";
+        }
+
+        [HttpDelete]
+        public string delete()
+        {
+            return "feito delete";
+        }
+
+    }
+}
